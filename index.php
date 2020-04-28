b7303a404d3d7e4d8422aeacfbb71027d99d3068
CodeFirmament <workjorge@yahoo.com>
2020-04-27 23:20:55 -0500

    added navbar using Bootstrap 4

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!--meta elements-->
+
+  <meta name="description" content="Find comfortable shoes with good deals!">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <link rel="shortcut icon" href="res/img/logos/shodotes.ico" type="image/x-icon">
+  <link rel="apple-touch-icon" href="res/img/logos/shodotes.ico">
+
+  <title>Shoes shop</title>
+
+  <!--Open Graph Protocol for Facebook-->
+
+  <meta property= "og:title" content="Sho.es : find your comfort">
+  <meta property= "og:description" content="Find good deals for shoes">
+  <meta property= "og:url" content="https://shoppingsho.es">
+  <meta property= "og:site_name" content="Shoes shop">
+  <meta property= "og:type" content="website">
+  <meta property= "og:image" content="url.png">
+
+  <!-- Twitter -->
+<meta name="twitter:card" content="summary">
+<meta name="twitter:site" content="@Shoes_shop_w">
+<meta name="twitter:creator" content="@Shoes_shop_w">
+<meta name="twitter:title" content="Sho.es: A ficticial shoes shop">
+<meta name="twitter:description" content="We are your place for good deals for shoes">
+<meta name="twitter:image" content="res/img/logos/shodotes.png">
+
+  <!--Bootstrap-->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+
+<body id="welcome-bg">
+  <nav class="navbar navbar-expand-sm bg-light navbar-light justify-content-center">
+      <a class="navbar-brand" href="#">
+        <img style="background-color:black" src="res/img/logos/shodotes.ico" width="30" height="30" class="d-inline-block align-top" alt="">
+        Sho.es
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#" title="Inicio">
+                Who we are
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="Publicaciones/2019/El-arte-ASCII.html" title="Publicaciones">
+                Shop
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="Biblioteca/index.html" title="Biblioteca">
+                Jobs
+            </a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="#" title="Noticias">
+                  Log In
+              </a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="#" title="Foros">
+                  Sign Up
+              </a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="#" title="Foros">
+                  Find Us
+              </a>
+          </li>
+        </ul>
+
+      </div>
+    </nav>
+
+    <Header>
+      <div class="container-fluid">
+        <h1 class= "justify-content-center">sho.es</h1>
+        <h2 class= "justify-content-center">We offer you the best shoes on your area</h2>
+      </div>
+    </Header>
+
+</body>
+</html>
